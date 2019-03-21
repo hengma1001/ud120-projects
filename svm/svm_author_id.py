5ce33124ab77787e4b3a11ecb399afe5b467b91c
Ma, Heng <mah1@ornl.gov>
2019-03-21 17:34:14 -0400

    optimize C param

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -27,21 +27,24 @@ labels_train = labels_train[:len(labels_train)/100]
 #########################################################
 ### your code goes here ###
 from sklearn.svm import SVC 
-clf = SVC(kernel='rbf') 
 
-t_train = time() 
-clf.fit(features_train, labels_train) 
-print "Training time", round(time()-t_train, 3), 's' 
-
-t_pred = time() 
-pred = clf.predict(features_test) 
-print "Predicting time", round(time()-t_pred, 3), 's' 
-
-
-from sklearn.metrics import accuracy_score
-acc = accuracy_score(pred, labels_test)
-
-print clf.score(features_test, labels_test), acc  
+C_list = [10., 100., 1000., 10000] 
+for C in C_list: 
+    clf = SVC(C=C, kernel='rbf') 
+    
+    t_train = time() 
+    clf.fit(features_train, labels_train) 
+    print "Training time for C=%f: " % C, round(time()-t_train, 3), 's' 
+    
+    t_pred = time() 
+    pred = clf.predict(features_test) 
+    print "Predicting time for C=%f: " % C , round(time()-t_pred, 3), 's' 
+    
+    
+    from sklearn.metrics import accuracy_score
+    acc = accuracy_score(pred, labels_test)
+    
+    print "Accuracy for C=%f: " % C, acc # clf.score(features_test, labels_test), acc  
 #########################################################
 
 
