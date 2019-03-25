f8d82c227b92c0ac886897b972d8b8a66f750a99
Ma, Heng <mah1@ornl.gov>
2019-03-25 16:37:33 -0400

    decision tree, less feature

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -24,8 +24,23 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.tree import DecisionTreeClassifier 
 
+clf = DecisionTreeClassifier(min_samples_split=40) 
+t_train = time() 
+clf.fit(features_train, labels_train) 
+print "Training time: ", round(time() - t_train, 3), "s" 
 
+t_pred = time() 
+pred = clf.predict(features_test) 
+print "Predicting time: ", round(time() - t_pred, 3), "s" 
+
+from sklearn.metrics import accuracy_score 
+acc = accuracy_score(pred, labels_test) 
+
+print "Resulting accuracy is ", acc 
+
+print "Number of features is ", len(features_train[0]) 
 #########################################################
 
 
