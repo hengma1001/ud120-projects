ac3e65c5c4e11ccee1685f1fd8acb5eb14d2c4b0
Ma, Heng <mah1@ornl.gov>
2019-03-27 11:40:13 -0400

    bonus vs salary

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -29,7 +29,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -37,11 +37,14 @@ test_color = "b"
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
+from sklearn.linear_model import LinearRegression 
+reg = LinearRegression()
+reg.fit(feature_train, target_train) 
 
-
-
-
-
+print "Regression slope: %.5f " % reg.coef_ 
+print "Regression intercept: %.5f " % reg.intercept_
+print "Score testing on training data: %.5f " % reg.score(feature_train, target_train) 
+print "Score testing on testing data: %.5f " % reg.score(feature_test, target_test) 
 
 
 
